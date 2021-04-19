d543ba82a29a8885247dac7b87d983826bfa35aa
denisCenter2m <47447276+DenisYavetsky@users.noreply.github.com>
2021-04-19 23:07:39 +0300

    Урок 1 дополнения

--- lesson1/task8.py
+++ lesson1/task8.py
@@ -5,4 +5,4 @@ year = int(input('Введите год: '))
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print('Год високосный')
 else:
-    print('Год невисокосный')
+    print('Год не високосный')
