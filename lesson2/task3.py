5f9cb43ef9bbed6c9fa09f7ea85e68f247114a3d
denisCenter2m <47447276+DenisYavetsky@users.noreply.github.com>
2021-04-22 22:47:51 +0300

    Урок 2 Задания

--- lesson2/task3.py
+++ lesson2/task3.py
@@ -1,3 +1,16 @@
 # Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран. Например,
 # если введено число 3486, то надо вывести число 6843
 
+num = int(input('Введите целое число: '))
+
+
+def num_recurse(num, s=0):
+    if num < 10:
+        return s * 10 + num
+    res = num % 10
+    num = num // 10
+    s = s * 10 + res
+    return num_recurse(num, s)
+
+
+print(num_recurse(num))
--- lesson2/task9.py
+++ lesson2/task9.py
@@ -3,3 +3,23 @@
 
 list_numbers = [int(i) for i in input('Введите числа и нажмите Enter').split()]
 
+
+def sum_recurse(num, s=0):
+    if num < 10:
+        return s + num
+    res = num % 10
+    num = num // 10
+    s += res
+    return sum_recurse(num, s)
+
+
+max_n = 0
+
+for number in list_numbers:
+
+    res = sum_recurse(number)
+    if res > max_n:
+        max_n = res
+        n = number
+
+print(f'Максимальное число по сумме цифр = {n}, его сумма = {max_n}')
